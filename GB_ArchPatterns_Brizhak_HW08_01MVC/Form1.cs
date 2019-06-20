d5236c71aebded0d9604f5d44c25c3ee24c55ab9
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-20 11:03:19 +0800

    Добавьте файлы проекта.

---
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GB_ArchPatterns_Brizhak_HW08_01MVC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GB_ArchPatterns_Brizhak_HW08_01MVC
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
