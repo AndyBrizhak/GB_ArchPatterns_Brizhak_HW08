fc44183c9361ac164af110ecce73346e8af06e71
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-22 16:10:08 +0800

    +public class ViewEvent : EventArgs

---
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/View/ViewEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_ArchPatterns_Brizhak_HW08_01MVC.View
+{
+    public class ViewEvent : EventArgs
+    {
+        public string _str;
+        public ViewEvent(string Str)
+        {
+            _str = Str;
+        }
+    }
+}
