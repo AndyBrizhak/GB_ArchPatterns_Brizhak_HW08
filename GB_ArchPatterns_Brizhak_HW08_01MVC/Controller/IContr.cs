772eb356468c451d13aee3652ff87c42c3325c1a
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-22 16:03:27 +0800

    +public interface IContr

---
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/Controller/IContr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_ArchPatterns_Brizhak_HW08_01MVC.Controller
+{
+   public interface IContr
+   {
+       void AddList();
+   }
+}
