07430bca67d20d87d263ff67d9d2d67e8e9f76fe
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-22 17:40:16 +0800

    change Program

--- GB_ArchPatterns_Brizhak_HW08_01MVC/Program.cs
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GB_ArchPatterns_Brizhak_HW08_01MVC.Controller;
+using GB_ArchPatterns_Brizhak_HW08_01MVC.Model;
 
 namespace GB_ArchPatterns_Brizhak_HW08_01MVC
 {
@@ -16,7 +18,11 @@ namespace GB_ArchPatterns_Brizhak_HW08_01MVC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MVCForm());
+
+            MVCForm view = new MVCForm();
+            IMod    mod = new Model.Model();
+            IContr contr = new Controller.Controller(view,mod);
+            Application.Run(view);
         }
     }
 }
