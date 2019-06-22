aa028b6eb723fc1a1d23c957068907f0844a72fa
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-22 17:18:31 +0800

    change class MVCForm

--- GB_ArchPatterns_Brizhak_HW08_01MVC/View/MVCForm.cs
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/View/MVCForm.cs
@@ -7,15 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GB_ArchPatterns_Brizhak_HW08_01MVC.Controller;
+using GB_ArchPatterns_Brizhak_HW08_01MVC.Model;
 using GB_ArchPatterns_Brizhak_HW08_01MVC.View;
 
 namespace GB_ArchPatterns_Brizhak_HW08_01MVC
 {
-    public partial class MVCForm : Form, IView, 
+    public partial class MVCForm : Form, IView, IFormObserver
     {
+        private IContr _controller;
+
         public MVCForm()
         {
             InitializeComponent();
+        }
+
+        public event ViewDelegate<IView> AddStr;
+
+        public void Change(IMod mod, ModelEvent e)
+        {
+            listBox.Items.Add(e.NewStr);
+        }
+
+        public void SetContr(IContr contr)
+        {
+            _controller = contr;
         }
     }
 }
