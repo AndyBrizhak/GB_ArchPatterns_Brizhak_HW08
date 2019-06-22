3f21f559538ffb4f1829bd6ad1d739d2d8f6f7bd
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-22 16:59:46 +0800

    +public class Controller : IContr

---
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/Controller/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GB_ArchPatterns_Brizhak_HW08_01MVC.Model;
+using GB_ArchPatterns_Brizhak_HW08_01MVC.View;
+
+namespace GB_ArchPatterns_Brizhak_HW08_01MVC.Controller
+{
+    public class Controller : IContr
+    {
+        IView _view;
+        IMod _model;
+
+        public Controller(IView view, IMod model)
+        {
+            _view = view ?? throw new ArgumentNullException(nameof(view));
+            _model = model ?? throw new ArgumentNullException(nameof(model));
+
+            _view.SetContr(this);
+            _model.AddObserver((IObserver) _view);
+            _view.AddStr += new ViewDelegate<IView>(this.ViewChange);
+        }
+
+        public void AddList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ViewChange(IView view, ViewEvent e)
+        {
+            _model.AddStr(e._str);
+        }
+    }
+}
