2265be698a737a575f5cd716eead7ea2ecd3532c
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-22 15:24:39 +0800

    +ModelEvent +IObserver +IMOD

---
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/Model/IObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_ArchPatterns_Brizhak_HW08_01MVC.Model
+{
+   public interface IObserver
+   {
+       void Change(IMod mod);
+   }
+}
---
+++ GB_ArchPatterns_Brizhak_HW08_01MVC/Model/ModelEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_ArchPatterns_Brizhak_HW08_01MVC.Model
+{
+    public delegate void ModDelegate<IMod>(IMod sender, ModelEvent e);
+
+   public class ModelEvent : EventArgs
+   {
+       public string NewStr;
+        public ModelEvent(string Str)
+        {
+            NewStr = Str;
+        }
+    }
+}
